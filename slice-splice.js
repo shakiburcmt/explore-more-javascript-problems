0a43b8bcc601d484e2a54e32ca0d39d656043d08
shakiburcmt <shakibur.cmt@gmail.com>
2022-08-10 16:54:57 +0600

    final

--- slice-splice.js
+++ slice-splice.js
@@ -1,9 +1,10 @@
 // array slice
-// const arra = [34, 4, 5, 6, 7, 4, 3, 122];
-// console.log(arra.slice(2, 5));
+const arra = [34, 4, 5, 6, 7, 4, 3, 122];
+console.log(arra.slice(2, 5));
 
 // array splice
 const array = [34, 4, 5, 6, 7, 4, 3, 122];
 const partial = array.splice(2, 4, 99, 100);
 // console.log(array.splice(2, 5, 123, 124, 125));
 console.log(partial);
+console.log(array);
